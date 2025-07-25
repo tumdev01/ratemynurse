1e7fbbff3fd5f00f1193a438fe869df65ebdd2b1
Tum <tumdev01@hotmail.com>
2025-07-26 00:36:22 +0700

    Create 2025_07_25_172255_create_user_profiles_table.php

---
+++ backend/laravel/database/migrations/2025_07_25_172255_create_user_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable(); // make nullable
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->string('religion');
+            $table->string('about')->nullable();
+            $table->string('gender')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('nationality')->default('THAI');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
