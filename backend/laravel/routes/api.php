4da69b1c1c99e0d43fb19d44a11320b881be908a
Tum <tumdev01@hotmail.com>
2025-08-21 01:44:59 +0700

    API get Provinces

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProvinceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +30,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     //Main Menu Nursing
     Route::get('nursing-locations', [NursingController::class, 'getLocations']);
+
+    Route::get('/provinces', [ProvinceController::class, 'getProvinces']);
 });
